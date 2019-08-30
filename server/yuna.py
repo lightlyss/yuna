22ed068af3ec2d81814f0d7651729c277dd1c1c8
Chara <37601555+lightlyss@users.noreply.github.com>
2019-08-30 10:49:56 -0400

    clean up endpoints

--- server/yuna.py
+++ server/yuna.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, redirect
 import requests
 import tensorflow as tf
 from afdetector import make_context, recognize
@@ -18,19 +18,22 @@ def downloadFile(url):
 # Routes ------------------------------------------------------------------------
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
+    return redirect('/static/index.html')
+
+@app.route('/api', methods=['GET'])
+def api():
     return jsonify({
         'service': 'yuna',
         'tensorflow': tf.__version__
     }), 200
 
-@app.route('/api/detect', methods=['GET'])
-def predict():
-    # if (not request.json or not 'url' in request.json):
-        # abort(400)
-    # imgPath = downloadFile(request.json['url'])
-    imgPath = downloadFile('https://pbs.twimg.com/media/EC-F0pFVUAAumMJ.jpg')
+@app.route('/api/detect', methods=['POST'])
+def detect():
+    if (not request.json or not 'url' in request.json):
+        abort(400)
+    imgPath = downloadFile(request.json['url'])
     if (imgPath is None):
         abort(502)
     res = recognize(context, imgPath)
