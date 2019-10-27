047d46c25ba8c725e2f54ee05e3c95342cd7d6fb
lightlyss <37601555+lightlyss@users.noreply.github.com>
2019-10-27 17:35:58 -0400

    update readme, add more tests

--- server/test_api.py
+++ server/test_api.py
@@ -8,6 +8,24 @@ class ApiTest(unittest.TestCase):
         res = self.app.get('/api')
         self.assertEqual(res.status_code, 200)
         self.assertIn('yuna', str(res.data))
+    def test_detect_no_url(self):
+        res = self.app.post('/api/detect', data={})
+        self.assertEqual(res.status_code, 400)
+    def test_detect_bad_url(self):
+        res = self.app.post('/api/detect', data={'url': 'https://github.com'})
+        self.assertEqual(res.status_code, 400)
+    def test_detect_bad_upstream(self):
+        res = self.app.post('/api/detect', data={
+            'url': 'http://localhost:3000/img/demo.png'
+        })
+        self.assertEqual(res.status_code, 502)
+    def test_detect(self):
+        res = self.app.post('/api/detect', data={
+            'url': 'https://raw.githubusercontent.com/lightlyss/yuna/master/demo.png'
+        })
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('bbox', str(res.data))
+        self.assertIn('score', str(res.data))
 
 def main():
     unittest.main()
