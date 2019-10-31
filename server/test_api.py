aeaa72996862023dbb935b7164485715a05bfc0d
lightlyss <37601555+lightlyss@users.noreply.github.com>
2019-10-30 22:00:40 -0400

    update client CI with tests

--- server/test_api.py
+++ server/test_api.py
@@ -4,21 +4,32 @@ from yuna import app
 class ApiTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
+
     def test_api(self):
         res = self.app.get('/api')
         self.assertEqual(res.status_code, 200)
         self.assertIn('yuna', str(res.data))
+
     def test_detect_no_url(self):
         res = self.app.post('/api/detect', json={})
         self.assertEqual(res.status_code, 400)
+
     def test_detect_bad_url(self):
         res = self.app.post('/api/detect', json={'url': 'https://github.com'})
         self.assertEqual(res.status_code, 400)
+
     def test_detect_bad_upstream(self):
         res = self.app.post('/api/detect', json={
-            'url': 'http://localhost:3000/img/demo.png'
+            'url': 'http://localhost:3000/img/banner.jpg'
         })
         self.assertEqual(res.status_code, 502)
+
+    def test_detect_security(self):
+        res = self.app.post('/api/detect', json={
+            'url': 'file:///yuna/static/index.png'
+        })
+        self.assertEqual(res.status_code, 502)
+
     def test_detect(self):
         res = self.app.post('/api/detect', json={
             'url': 'https://raw.githubusercontent.com/lightlyss/yuna/master/banner.png'
