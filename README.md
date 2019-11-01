506d742c48b07d00e2adde1cec558f6e408608db
lightlyss <37601555+lightlyss@users.noreply.github.com>
2019-10-31 23:23:16 -0400

    revise end-user output (#5)

--- README.md
+++ README.md
@@ -10,10 +10,15 @@ TensorFlow. Comes packaged with a Discord bot.
 
 ![demo](demo.png)
 
-## Development
+## Development (Docker)
 Specify `YUNA_TOKEN` inside a `.env` file or as a plain environment variable for
-the Discord bot. The system is provided as a ready-to-use docker network.
+the Discord bot. The system is provided as a ready-to-use Docker network.
 `docker-compose up -d` to start. The server component will be available at port `5000`.
+The client component can be reached through Discord using commands such as
+`!detect <IMG_URL>`. Processing can take a long time, depending on the system hosting the
+server.
+
+## Development (Native)
 Running natively is presently untested, but to do so requires Python 3 and TensorFlow
 in addition to Python modules listed in `requirements.txt` manifests. Relevant models
 must also be recompiled manually before the server can function natively:
--- client/yuna.py
+++ client/yuna.py
@@ -15,11 +15,11 @@ async def detect(ctx, *args):
         url = args[0]
     else:
         return None
-    await ctx.send(f'Observing at [{url}]...')
+    await ctx.send(f'Attempting to observe «{getFname(url)}»...')
 
     result = afd(url)
     if (result == Code.EUPSTREAM):
-        return await ctx.send(f'[{url}] is unsupported!')
+        return await ctx.send(f'«{getFname(url)}» is unsupported!')
 
     unknowns = 0
     total = len(result)
@@ -31,13 +31,13 @@ async def detect(ctx, *args):
         elif (file is None):
             file = discord.File(item, filename=getFname(item))
             embed = discord.Embed(
-                title=f'1st of {total}',
+                title=f'Detected {total} character(s)',
                 type='rich',
-                description=f'{unknowns} of {total} were low confidence'
+                description=f'{total - unknowns} of {total} identifiable.'
             ).set_image(url=f'attachment://{getFname(item)}')
 
     if (file is None):
-        return await ctx.send(f'Identified {total} results, of which {unknowns} were low confidence.')
+        return await ctx.send(f'Detected {total} character(s), {unknowns} not identifiable.')
     return await ctx.send(file=file, embed=embed)
 
 bot.run(TOKEN)
